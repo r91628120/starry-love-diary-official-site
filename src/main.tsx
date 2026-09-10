a248e86c721c034ce3f6235a98a555179eae13d1
r91628120 <r91628120@gmail.com>
2026-09-10 15:01:52 +0800

    Load app artwork refinements

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './styles/site.css'
+import './styles/brand-art.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App page={document.body.dataset.page === 'privacy' ? 'privacy' : document.body.dataset.page === 'terms' ? 'terms' : 'home'} /></StrictMode>)
