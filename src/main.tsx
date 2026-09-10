70bd3b6c0913a4756971f0112c8386008a789457
r91628120 <r91628120@gmail.com>
2026-09-10 20:38:53 +0800

    Load final black cat callout polish

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import './styles/site.css'
 import './styles/brand-art.css'
+import './styles/final-cat-card.css'
 
 createRoot(document.getElementById('root')!).render(<StrictMode><App page={document.body.dataset.page === 'privacy' ? 'privacy' : document.body.dataset.page === 'terms' ? 'terms' : 'home'} /></StrictMode>)
